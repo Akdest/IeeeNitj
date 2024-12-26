4ca234952b68e3be0c93ccc81af6e11139d80d24
vashishtJatin <142159476+vashishtJatin@users.noreply.github.com>
2024-12-26 12:37:50 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import NewsEvents from '../../components/NewsEvents'
+import Herosection from '../../components/Herosection'
+import Gallery from '../../components/Gallery'
 
 export default function Home() {
   return (
     <>
     <NewsEvents/>
+    <Herosection/>
+    <Gallery/>
     </>
   );
 }
