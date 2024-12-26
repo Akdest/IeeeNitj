23afd849d826bd6be2d003af404cdd386569d75f
Kushagra Saxena <154624873+sxnkush@users.noreply.github.com>
2024-12-26 19:31:44 +0530

    Revert "Added Navigation and footer"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Footer from '../../components/Footer';
-import NavigationBar from '../../components/Navigation';
 import NewsEvents from '../../components/NewsEvents'
 
 export default function Home() {
   return (
     <>
-    <NavigationBar login={false}/>
     <NewsEvents/>
-    <Footer/>
     </>
   );
 }
