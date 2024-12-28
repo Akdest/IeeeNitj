e134245181ba3c6b84cd49551917ad9e73acbb3c
Jatin Vashisht <142159476+vashishtJatin@users.noreply.github.com>
2024-12-28 11:53:04 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Footer from './components/Footer';
 import NavigationBar from './components/Navigation';
-import NewsEvents from '../../components/NewsEvents'
+import NewsEvents from '../../components/NewsEvents';
+import Herosection from '../Components/Herosection'
+import Gallery from '../Components/Gallery'
 
 export default function Home() {
   return (
     <>
     <NavigationBar login={false}/>
+    <Herosection />
     <NewsEvents/>
+    <Gallery/>
     <Footer/>
     </>
   );
