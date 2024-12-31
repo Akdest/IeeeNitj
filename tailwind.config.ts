bcdc50675e43681d3b730986b696a2321e2081e9
sxnkush <saxenakushagra198@gmail.com>
2024-12-31 20:22:41 +0530

    Navigation Updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,27 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        typing: {
+          '0%': { width: '0%' },
+          '80%': { width: '100%' }, 
+          '100%': { width: '100%' },
+        },
+        blink: {
+          '0%, 100%': { borderColor: 'transparent' },
+          '50%': { borderColor: 'black' },
+        },
+        vibrate:{
+          '0%':{top:"0%"},
+          '50%':{top:"1%"},
+          '100%':{top:"0%"}
+        }
+      },
+      animation: {
+        typing: 'typing 4s steps(20, end) infinite',
+        blink: 'blink 0.5s step-end infinite',
+        vibrate:'vibrate 0.8s linear infinite'
+      },
     },
   },
   plugins: [],
