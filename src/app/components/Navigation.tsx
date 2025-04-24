c7c6e485d5eb420121d6da06fbd62af3885876f8
Ayush Kumar <ayushkr0104@hotmail.com>
2025-04-24 22:37:39 +0530

    nav readd

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -2,8 +2,10 @@
 import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
+// import { link } from "fs";
 
-export default function NavigationBar() {
+export default function NavigationBar({ login = false }) {
+  const log = login ? "Log Out" : "Log In";
   const [hamburger, setHamburger] = useState(false);
   const [activeSubMenu, setActiveSubMenu] = useState<string | null>(null);
   const [scrolled, setScrolled] = useState(false);
@@ -75,17 +77,18 @@ export default function NavigationBar() {
               { name: "Home", link: "/" },
               { name: "About Us", link: "/AboutUs" },
               { name: "Events", link: "/Event" },
-              { name: "News", link: "/News" },
+              { name: "News", link:"/News"},
               { name: "Gallery", link: "/Gallery" },
               {
                 name: "Committee",
                 subMenu: true,
                 subValues: [
-                  { nameSub: "Secretary", linkSub: "/Commitee" },
-                  { nameSub: "Student", linkSub: "/Commitee" },
+                  { nameSub: "Secretary", linkSub: "#" },
+                  { nameSub: "Student", linkSub: "#" },
                 ],
               },
               { name: "Contacts", link: "/Contact" },
+
             ].map(({ name, link, subMenu }, idx) => (
               <li key={idx}>
                 {subMenu ? (
@@ -169,7 +172,9 @@ export default function NavigationBar() {
             {[
               { name: "Home", link: "/" },
               { name: "Events", link: "/Event" },
-              { name: "News", link: "/News" },
+              {
+                name: "News",  link: "/News"
+              },
               { name: "Gallery", link: "/Gallery" },
               {
                 name: "Committee",
@@ -180,6 +185,7 @@ export default function NavigationBar() {
                 ],
               },
               { name: "Contacts", link: "/Contact" },
+              { name: log, link: "#" },
             ].map(({ name, link, subMenu, subValues }, idx) => (
               <li key={idx}>
                 {subMenu ? (
